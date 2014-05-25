88c6b8bcb8c885cc9041709301aba21c2c4f3aa8
Dan Lew <danlew42@gmail.com>
2014-05-25 16:42:27 -0500

    It works!

--- Gfycat/src/main/java/net/danlew/gfycat/ui/MainActivity.java
+++ Gfycat/src/main/java/net/danlew/gfycat/ui/MainActivity.java
@@ -1,9 +1,14 @@
 package net.danlew.gfycat.ui;
 
 import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import net.danlew.gfycat.GfycatApplication;
@@ -12,15 +17,17 @@ import net.danlew.gfycat.model.ConvertGif;
 import net.danlew.gfycat.model.UrlCheck;
 import net.danlew.gfycat.service.GfycatService;
 import rx.Observable;
+import rx.Subscription;
 import rx.android.observables.AndroidObservable;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.functions.Func2;
 import rx.schedulers.Schedulers;
-import rx.subscriptions.CompositeSubscription;
+import rx.subjects.BehaviorSubject;
 
 import javax.inject.Inject;
 
-
+// TODO: Handle rotation
 public class MainActivity extends Activity {
 
     @Inject
@@ -29,7 +36,12 @@ public class MainActivity extends Activity {
     @InjectView(R.id.progress_bar)
     ProgressBar mProgressBar;
 
-    private CompositeSubscription mCompositeSubscription = new CompositeSubscription();
+    @InjectView(R.id.video_view)
+    TextureView mVideoView;
+
+    private Subscription mGetNameSubscription;
+
+    private BehaviorSubject<SurfaceTexture> mSurfaceTextureSubject = BehaviorSubject.create((SurfaceTexture) null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.inject(this);
+
+        mVideoView.setSurfaceTextureListener(mSurfaceTextureListener);
 
         if (savedInstanceState == null) {
             final String url = getIntent().getData().toString();
@@ -60,18 +74,73 @@ public class MainActivity extends Activity {
                             });
                         }
                     }
+                )
+                .flatMap(
+                    new Func1<String, Observable<? extends String>>() {
+                        @Override
+                        public Observable<? extends String> call(String gfyName) {
+                            // Error out if the name is empty
+                            if (TextUtils.isEmpty(gfyName)) {
+                                return Observable.error(new RuntimeException("Could not get gfyName for url: " + url));
+                            }
+
+                            return Observable.just(gfyName);
+                        }
+                    }
                 );
 
-            mCompositeSubscription.add(
-                AndroidObservable.bindActivity(this, gfyNameObservable)
-                    .subscribeOn(Schedulers.io())
-                    .subscribe(new Action1<String>() {
-                        @Override
-                        public void call(String gfyName) {
-                            Toast.makeText(MainActivity.this, "gfyName=" + gfyName, Toast.LENGTH_SHORT).show();
+            Observable<MediaPlayer> readyForDisplayObservable = Observable.combineLatest(
+                gfyNameObservable,
+                mSurfaceTextureSubject,
+                new Func2<String, SurfaceTexture, MediaPlayer>() {
+                    @Override
+                    public MediaPlayer call(String gfyName, SurfaceTexture surfaceTexture) {
+                        if (TextUtils.isEmpty(gfyName) || surfaceTexture == null) {
+                            return null;
                         }
-                    })
-            );
+
+                        MediaPlayer mediaPlayer = new MediaPlayer();
+
+                        try {
+                            mediaPlayer.setDataSource("http://zippy.gfycat.com/" + gfyName + ".webm");
+                            mediaPlayer.setSurface(new Surface(mVideoView.getSurfaceTexture()));
+                        }
+                        catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        return mediaPlayer;
+                    }
+                }
+            ).filter(new Func1<MediaPlayer, Boolean>() {
+                @Override
+                public Boolean call(MediaPlayer mediaPlayer) {
+                    return mediaPlayer != null;
+                }
+            });
+
+            mGetNameSubscription = AndroidObservable.bindActivity(this, readyForDisplayObservable)
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Action1<MediaPlayer>() {
+                    @Override
+                    public void call(MediaPlayer mediaPlayer) {
+                        try {
+                            mediaPlayer.setLooping(true);
+                            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                                @Override
+                                public void onPrepared(MediaPlayer mp) {
+                                    mProgressBar.setVisibility(View.GONE);
+                                    mp.start();
+                                }
+                            });
+
+                            mediaPlayer.prepareAsync();
+                        }
+                        catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
         }
     }
 
@@ -79,6 +148,29 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
 
-        mCompositeSubscription.unsubscribe();
+        mGetNameSubscription.unsubscribe();
     }
+
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            mSurfaceTextureSubject.onNext(surface);
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            mSurfaceTextureSubject.onNext(null);
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
 }
