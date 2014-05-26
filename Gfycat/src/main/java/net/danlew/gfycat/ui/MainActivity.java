a42efb8f05b97599aa83b7f191f46e0c57797135
Dan Lew <danlew42@gmail.com>
2014-05-25 19:59:58 -0500

    Properly clean-up/restart onStop

--- Gfycat/src/main/java/net/danlew/gfycat/ui/MainActivity.java
+++ Gfycat/src/main/java/net/danlew/gfycat/ui/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.SurfaceTexture;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
@@ -45,11 +44,17 @@ public class MainActivity extends Activity {
     @InjectView(R.id.error_text_view)
     TextView mErrorTextView;
 
+    private GfyMetadata mGfyMetadata;
+    private boolean mLoadFailed;
+
     private MediaPlayer mMediaPlayer;
 
-    private Subscription mGetNameSubscription;
+    private Subscription mLoadVideoSubscription;
 
     private BehaviorSubject<SurfaceTexture> mSurfaceTextureSubject = BehaviorSubject.create((SurfaceTexture) null);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Lifecycle
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,126 +67,178 @@ public class MainActivity extends Activity {
         ButterKnife.inject(this);
 
         mVideoView.setSurfaceTextureListener(mSurfaceTextureListener);
+    }
 
-        if (savedInstanceState == null) {
-            final String url = getIntent().getData().toString();
-            Observable<GfyMetadata> gfyNameObservable = mGfycatService.checkUrl(url)
-                .flatMap(
-                    new Func1<UrlCheck, Observable<String>>() {
-                        @Override
-                        public Observable<String> call(UrlCheck urlCheck) {
-                            if (urlCheck.isUrlKnown()) {
-                                return Observable.just(urlCheck.getGfyName());
-                            }
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-                            return mGfycatService.convertGif(url).map(new Func1<ConvertGif, String>() {
-                                @Override
-                                public String call(ConvertGif convertGif) {
-                                    return convertGif.getGfyName();
-                                }
-                            });
-                        }
-                    }
-                )
-                .flatMap(
-                    new Func1<String, Observable<? extends String>>() {
-                        @Override
-                        public Observable<? extends String> call(String gfyName) {
-                            // Error out if the name is empty
-                            if (TextUtils.isEmpty(gfyName)) {
-                                return Observable.error(new RuntimeException("Could not get gfyName for url: " + url));
-                            }
+        // Always begin load in onStart(); we stop MediaPlayer in onStop() every time
+        loadGfy();
+    }
 
-                            return Observable.just(gfyName);
-                        }
-                    }
-                )
-                .flatMap(new Func1<String, Observable<? extends GfyMetadata>>() {
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Reset the state of the entire Activity, prepare to reload everything in onStart()
+        mLoadVideoSubscription.unsubscribe();
+
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+
+        mProgressBar.setVisibility(View.VISIBLE);
+        mErrorTextView.setVisibility(View.GONE);
+        // TODO: Figure out how to reset TextureViews
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    // RxJava
+
+    private Observable<GfyMetadata> getGfyMetadataObservable() {
+        if (mGfyMetadata != null) {
+            return Observable.just(mGfyMetadata);
+        }
+
+        final String url = getIntent().getData().toString();
+
+        if (mLoadFailed) {
+            return Observable.error(new RuntimeException("Could not load gfy metadata for " + url));
+        }
+
+        return mGfycatService.checkUrl(url)
+            .flatMap(
+                new Func1<UrlCheck, Observable<String>>() {
                     @Override
-                    public Observable<? extends GfyMetadata> call(String gfyName) {
-                        return mGfycatService.getMetadata(gfyName);
-                    }
-                });
-
-            Observable<MediaPlayer> readyForDisplayObservable = Observable.combineLatest(
-                gfyNameObservable,
-                mSurfaceTextureSubject,
-                new Func2<GfyMetadata, SurfaceTexture, MediaPlayer>() {
-                    @Override
-                    public MediaPlayer call(GfyMetadata gfyMetadata, SurfaceTexture surfaceTexture) {
-                        if (gfyMetadata == null || surfaceTexture == null) {
-                            return null;
+                    public Observable<String> call(UrlCheck urlCheck) {
+                        if (urlCheck.isUrlKnown()) {
+                            return Observable.just(urlCheck.getGfyName());
                         }
 
-                        MediaPlayer mediaPlayer = new MediaPlayer();
-
-                        try {
-                            mediaPlayer.setDataSource(gfyMetadata.getGfyItem().getWebmUrl());
-                            mediaPlayer.setSurface(new Surface(mVideoView.getSurfaceTexture()));
-                        }
-                        catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-
-                        return mediaPlayer;
+                        return mGfycatService.convertGif(url).map(new Func1<ConvertGif, String>() {
+                            @Override
+                            public String call(ConvertGif convertGif) {
+                                return convertGif.getGfyName();
+                            }
+                        });
                     }
                 }
-            ).filter(new Func1<MediaPlayer, Boolean>() {
+            )
+            .flatMap(
+                new Func1<String, Observable<? extends String>>() {
+                    @Override
+                    public Observable<? extends String> call(String gfyName) {
+                        // Error out if the name is empty
+                        if (TextUtils.isEmpty(gfyName)) {
+                            return Observable.error(new RuntimeException("Could not get gfyName for url: " + url));
+                        }
+
+                        return Observable.just(gfyName);
+                    }
+                }
+            )
+            .flatMap(new Func1<String, Observable<? extends GfyMetadata>>() {
+                @Override
+                public Observable<? extends GfyMetadata> call(String gfyName) {
+                    return mGfycatService.getMetadata(gfyName);
+                }
+            })
+            .doOnNext(new Action1<GfyMetadata>() {
+                @Override
+                public void call(GfyMetadata gfyMetadata) {
+                    mGfyMetadata = gfyMetadata;
+                }
+            })
+            .doOnError(new Action1<Throwable>() {
+                @Override
+                public void call(Throwable throwable) {
+                    mLoadFailed = true;
+                }
+            });
+    }
+
+    private Observable<MediaPlayer> getLoadMediaPlayerObservable(Observable<GfyMetadata> gfyMetadataObservable) {
+        return Observable.combineLatest(gfyMetadataObservable, mSurfaceTextureSubject,
+            new Func2<GfyMetadata, SurfaceTexture, MediaPlayer>() {
+                @Override
+                public MediaPlayer call(GfyMetadata gfyMetadata, SurfaceTexture surfaceTexture) {
+                    if (gfyMetadata == null || surfaceTexture == null) {
+                        return null;
+                    }
+
+                    MediaPlayer mediaPlayer = new MediaPlayer();
+
+                    mediaPlayer.setLooping(true);
+
+                    mediaPlayer.setOnVideoSizeChangedListener(mAspectRatioListener);
+
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mProgressBar.setVisibility(View.GONE);
+                            mp.start();
+                        }
+                    });
+
+                    try {
+                        mediaPlayer.setDataSource(gfyMetadata.getGfyItem().getWebmUrl());
+                        mediaPlayer.setSurface(new Surface(mVideoView.getSurfaceTexture()));
+                    }
+                    catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+
+                    return mediaPlayer;
+                }
+            }
+        )
+            .filter(new Func1<MediaPlayer, Boolean>() {
                 @Override
                 public Boolean call(MediaPlayer mediaPlayer) {
                     return mediaPlayer != null;
                 }
+            })
+            .doOnNext(new Action1<MediaPlayer>() {
+                @Override
+                public void call(MediaPlayer mediaPlayer) {
+                    mMediaPlayer = mediaPlayer;
+                }
             });
+    }
 
-            mGetNameSubscription = AndroidObservable.bindActivity(this, readyForDisplayObservable)
-                .subscribeOn(Schedulers.io())
-                .subscribe(
-                    new Action1<MediaPlayer>() {
-                        @Override
-                        public void call(MediaPlayer mediaPlayer) {
-                            try {
-                                mMediaPlayer = mediaPlayer;
+    private void loadGfy() {
+        Observable<GfyMetadata> gfyMetadataObservable = getGfyMetadataObservable();
+        Observable<MediaPlayer> readyForDisplayObservable = getLoadMediaPlayerObservable(gfyMetadataObservable);
 
-                                mediaPlayer.setLooping(true);
-
-                                mediaPlayer.setOnVideoSizeChangedListener(mAspectRatioListener);
-
-                                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                                    @Override
-                                    public void onPrepared(MediaPlayer mp) {
-                                        mProgressBar.setVisibility(View.GONE);
-                                        mp.start();
-                                    }
-                                });
-
-                                mediaPlayer.prepareAsync();
-                            }
-                            catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
+        mLoadVideoSubscription = AndroidObservable.bindActivity(this, readyForDisplayObservable)
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                new Action1<MediaPlayer>() {
+                    @Override
+                    public void call(MediaPlayer mediaPlayer) {
+                        try {
+                            mediaPlayer.prepareAsync();
                         }
-                    },
-                    new Action1<Throwable>() {
-                        @Override
-                        public void call(Throwable throwable) {
-                            mProgressBar.setVisibility(View.GONE);
-                            mErrorTextView.setVisibility(View.VISIBLE);
+                        catch (Exception e) {
+                            throw new RuntimeException(e);
                         }
                     }
-                );
-        }
+                },
+                new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        mProgressBar.setVisibility(View.GONE);
+                        mErrorTextView.setVisibility(View.VISIBLE);
+                    }
+                }
+            );
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        mGetNameSubscription.unsubscribe();
-
-        if (mMediaPlayer != null) {
-            mMediaPlayer.release();
-        }
-    }
+    //////////////////////////////////////////////////////////////////////////
+    // Listeners
 
     private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
         @Override
